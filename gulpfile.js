03c2ac567ca8bc8495940ad11405d805376397c9
Adam Morgan <atom.morgan@gmail.com>
2017-04-01 12:52:46 -0400

    Add gulp task for watching TS files

--- gulpfile.js
+++ gulpfile.js
@@ -17,3 +17,17 @@ gulp.task('move-test-config', function() {
   return gulp.src(['./src/test/mocha.opts'])
     .pipe(gulp.dest('built/test'));
 });
+
+gulp.task('scripts', function() {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('built'));
+});
+
+gulp.task('watch', ['scripts'], function() {
+  gulp.watch([
+    './src/*.ts',
+    './src/**/*.ts',
+    './src/**/**/*.ts',
+  ], ['scripts']);
+});

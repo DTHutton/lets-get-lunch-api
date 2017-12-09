d05c77afc23c35ab8a4d5bd9f82d1a48bdeed2f7
Adam Morgan <atom.morgan@gmail.com>
2017-12-08 20:21:28 -0500

    Remove todo

--- src/models/event/event.ts
+++ src/models/event/event.ts
@@ -1,7 +1,6 @@
 import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-// TODO add preferences - event specific if suggestLocations isn't true
 const EventSchema = new mongoose.Schema({
   _creator: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
   title: { type: String, required: true },

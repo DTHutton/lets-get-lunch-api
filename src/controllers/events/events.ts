41b75fc0fe73cddaf858f4f50d78e45d6690d870
Adam Morgan <atom.morgan@gmail.com>
2017-12-12 20:56:52 -0500

    Update error message for all events endpoint

--- src/controllers/events/events.ts
+++ src/controllers/events/events.ts
@@ -64,7 +64,7 @@ function all(req, res) {
       res.status(200).json(events);
     })
     .catch((err) => {
-      res.status(500).json(err);
+      res.status(500).json({ message: 'Something went wrong!' });
     });
 }
 

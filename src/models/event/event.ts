86ee5b3952f9afc247f09cbb3d50be40daa7bfa1
Adam Morgan <atom.morgan@gmail.com>
2018-02-22 09:01:03 -0500

    Add pre save hook for events

--- src/models/event/event.ts
+++ src/models/event/event.ts
@@ -1,4 +1,5 @@
 import * as mongoose from 'mongoose';
+import * as moment from 'moment';
 const Schema = mongoose.Schema;
 
 const EventSchema = new mongoose.Schema({
@@ -11,6 +12,14 @@ const EventSchema = new mongoose.Schema({
   startTime: { type: Date, required: true },
   endTime: { type: Date, required: true },
   suggestLocations: { type: Boolean, default: false }
+});
+
+EventSchema.pre('save', function(next) {
+  if (moment(this.endTime).isBefore(moment(this.startTime))) {
+    return next(new Error('Your start date must be before the end date.'));
+  } else {
+    return next();
+  }
 });
 
 export default mongoose.model('Event', EventSchema);
